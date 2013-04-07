fdbb82537f93b91c9d352c92486ea713e8387ccd
Suraj <suraj02@gmail.com>
2013-04-07 16:03:59 +0530

    Finished for first release

--- contact.php
+++ contact.php
@@ -1,3 +1,5 @@
 <?php
 //Contact information goes here..
+
+// would show a form so users can fill it to submit details
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 // This project is used to define the services and contact info...
+
+//Show Links to Service and Contact Pages
+
+Services goes here
+
+Contact goes here
 ?>
